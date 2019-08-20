4de08fd07b0c674c0422977da1eef346967d96c1
Sam McCormack <sampmccormack@gmail.com>
2019-08-20 11:49:07 +0100

    Fix package installer script

--- packages/install.py
+++ packages/install.py
@@ -42,17 +42,22 @@ def check_cwd():
         sys.exit(1)
 
     os.chdir(new_cwd)
+    return new_cwd
 
 
 if __name__ == "__main__":
-    check_cwd()
+    wd = check_cwd()
     files = []
 
     for file in glob("**/setup.py", recursive=True):
         if "for_redistribution_files_only" in file:
-            files.append(file)
-            os.system(f"python3 {file} install")
-            os.system(f"python {file} install")
+            files.append(os.path.abspath(file))
+           
+    for f in files:
+        os.chdir(wd)
+        os.chdir("/".join(f.split("/")[:-1]))
+        os.system(f"python3 setup.py install")
+        os.system(f"python setup.py install")
 
-    print("\n\n")
+    print("\n")
     print("\n".join([f"Attempted to install package from {f}" for f in files]))
