00309153b818f2a81761d89072931ab077d60e4f
Sam McCormack <sam.p.mccormack@gmail.com>
2019-10-03 15:20:16 +0100

    Install script supports different python versions

--- packages/install.py
+++ packages/install.py
@@ -4,11 +4,17 @@
 Python script which installs all the custom packages from this directory and also installs dependencies via pip.
 """
 import os
+import platform
 import sys
 from glob import glob
 
 packages = "packages"
 cwd = os.getcwd()
+
+python = "python"
+
+
+def pip(): return f"{python} -m pip"
 
 
 def find_cwd():
@@ -32,6 +38,27 @@ def check_cwd():
 
 
 if __name__ == "__main__":
+    is_windows = "Windows" == platform.system()
+    args = sys.argv
+
+    if not is_windows and len(args) == 1:
+        print("Using 'python3' instead of 'python'. If this is not desired, call the script with 'python' as an argument.")
+        python = "python3"  # For *nix systems, python3 is required.
+
+    # If a particular version is specified, e.g. "python3.8".
+    elif len(args) > 1:
+        arg = args[1]
+        if len(args) > 2:
+            print("ERROR: too many arguments.")
+            sys.exit(1)
+        if "python" not in arg:
+            print(
+                f"ERROR: arg '{arg}' does not appear to match a python version.")
+            if input("Continue anyway? (y/N) ").lower() != "y":
+                sys.exit(1)
+
+        python = arg
+
     wd = check_cwd()
     files = []
 
@@ -42,13 +69,12 @@ if __name__ == "__main__":
     for f in files:
         os.chdir(wd)
         os.chdir("/".join(f.replace("\\", "/").split("/")[:-1]))
-        os.system(f"python3 setup.py install")
-        os.system(f"python setup.py install")
+        os.system(f"{python} setup.py install")
 
     os.chdir(f"{wd}/..")
-    os.system("pip install -r requirements.txt")
-    os.system("pip3 install -r requirements.txt")
+    os.system(f"{pip()} install -r requirements.txt")
 
     print("\n")
+    print(f"\nInstalled dependencies for {python}.")
     print("\n".join([f"Attempted to install package from {f}" for f in files]))
     print("\nInstalled pip dependencies from requirements.txt")
