4ccfe76f0a079200a3beb903a1ba71b35aedfa5e
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2019-10-18 18:05:03 +0100

    Install script uses correct interpreter

--- packages/install.py
+++ packages/install.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 
 """
-Python script which installs all the custom packages from this directory and also installs dependencies via pip.
+Python script which installs all Matlab packages and pip dependencies.
 """
+
 import os
 import platform
+import subprocess
 import sys
+import time
 from glob import glob
+from typing import Tuple, Optional
 
 packages = "packages"
 cwd = os.getcwd()
 
-python = "python"
 
-
-def pip(): return f"{python} -m pip"
-
-
-def find_cwd():
+def find_cwd() -> Optional[str]:
     if packages in os.listdir("."):
         return os.path.join(cwd, packages)
     elif cwd.replace("\\", "/").split("/")[-1] == packages:
@@ -25,59 +24,92 @@ def find_cwd():
     return None
 
 
-def check_cwd():
+def check_cwd() -> str:
     new_cwd = find_cwd()
 
     if not new_cwd:
-        print("ERROR: The script should be executed from the "
-              "PyMODA directory or its `packages` subfolder.")
-        sys.exit(1)
+        fatal_error(
+            "The script should be executed from the PyMODA directory or its `packages` subfolder."
+        )
 
     os.chdir(new_cwd)
     return new_cwd
 
 
+def run_command(command: str, verbose=False) -> Tuple[str, str]:
+    print(f"Running command: {command}")
+
+    result = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+        universal_newlines=True,
+    )
+    output = result.stdout
+    error = result.stderr
+
+    if verbose:
+        print(output, end="\n\n")
+        if error:
+            print(error, end="\n\n")
+
+    return output, error
+
+
+def fatal_error(message: str):
+    # ANSI codes to make the text red.
+    red, endc = "\033[91m", "\033[0m"
+
+    print(f"\n{red}Error: {message}{endc}")
+    sys.exit(1)
+
+
 if __name__ == "__main__":
+    assert sys.version_info >= (3, 6)
+
+    # Use verbose mode when "-v" is provided as an argument.
+    verbose = len(sys.argv) > 1 and "v" in sys.argv[1].lower()
+    if verbose:
+        print("Launched in verbose mode.\n")
+        time.sleep(1)
+    else:
+        print(
+            "Launched in normal mode. Use command-line argument '-v' to launch in verbose mode.\n"
+        )
+
+    # Whether the OS is Windows-based.
     is_windows = "Windows" == platform.system()
-    args = sys.argv
 
-    if not is_windows and len(args) == 1:
-        print("Using 'python3' instead of 'python'. If this is not desired, call the script with 'python' as an argument.")
-        python = "python3"  # For *nix systems, python3 is required.
+    python = sys.executable  # Path to correct python interpreter.
+    pip = f"{python} -m pip"  # Pip command for current interpreter.
 
-    # If a particular version is specified, e.g. "python3.8".
-    elif len(args) > 1:
-        arg = args[1]
-        if len(args) > 2:
-            print("ERROR: too many arguments.")
-            sys.exit(1)
-        if "python" not in arg:
-            print(
-                f"ERROR: arg '{arg}' does not appear to match a python version.")
-            if input("Continue anyway? (y/N) ").lower() != "y":
-                sys.exit(1)
-
-        python = arg
+    if " " in python:
+        python = f'"{python}"'  # If Python path contains spaces, use quotes.
 
     wd = check_cwd()
     files = []
 
+    # Get all the `setup.py` files for MATLAB packages.
     for file in glob("**/setup.py", recursive=True):
         if "for_redistribution_files_only" in file:
             files.append(os.path.abspath(file))
-
+ 
     for f in files:
         os.chdir(wd)
         os.chdir("/".join(f.replace("\\", "/").split("/")[:-1]))
-        os.system(f"{python} setup.py install")
+
+        out, err = run_command(f"{python} setup.py install", verbose=verbose)
+        if "Permission denied" in err:
+            fatal_error(
+                "Install script must be run with elevated permissions. "
+                "Use an administrator terminal on Windows or prefix the command with 'sudo' on macOS/Linux."
+            )
 
     os.chdir(f"{wd}/..")
-    os.system(f"{pip()} install -r requirements.txt")
+    run_command(f"{pip} install -r requirements.txt", verbose=verbose)
 
     if is_windows:
-        os.system(f"{pip()} install winshell pypiwin32")
+        run_command(f"{pip} install winshell pypiwin32", verbose=verbose)
 
-    print("\n")
-    print(f"\nInstalled dependencies for {python}.")
-    print("\n".join([f"Attempted to install package from {f}" for f in files]))
-    print("\nInstalled pip dependencies from requirements.txt")
+    print("\nInstall script has finished.")
