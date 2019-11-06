6f8178ad5627dd77db806e9574150eec8fb1be43
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2019-11-06 10:06:51 +0000

    Add tshift surrogate

--- src/maths/algorithms/surrogates.py
+++ src/maths/algorithms/surrogates.py
@@ -13,7 +13,8 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
-from typing import Union
+import random
+from typing import Union, Tuple
 
 import numpy as np
 from numpy import ndarray
@@ -24,7 +25,12 @@ from maths.signals.TimeSeries import TimeSeries
 """
 Translation of `surrcalc` from MODA.
 
-STATUS: Some surrogate types implemented and may be working. 
+STATUS: 
+- RP, FT, AAFT, IAFFT1, tshift and CPP surrogates are implemented.
+- IAFFT1 needs to be fixed (it crashes). 
+- IAAFT is not implemented yet.
+- WIAAFT requires a function "modwt" which is not implemented in scipy. Not implemented yet.
+- Results may need to be checked for surrogates. 
 """
 
 _RP = "RP"
@@ -37,7 +43,9 @@ _tshift = "tshift"
 _CPP = "CPP"
 
 
-def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
+def surrogate_calc(
+    time_series: Union[TimeSeries, ndarray], N: int, method: str, pp: bool, fs: float
+):
     """
     Calculates surrogates.
 
@@ -69,7 +77,7 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
         params.sigstart = ks
         params.sigend = ke
     else:
-        time = np.linspace(0, len(sig), len(sig) / fs)
+        time = np.linspace(0, len(sig), int(len(sig) / fs))
         params.preprocessing = False
 
     L = len(sig)
@@ -77,10 +85,12 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
 
     params.time = time
 
+    # Random permutation surrogates.
     if method == _RP:
         for k in range(N):
             surr[k, :] = sig[randperm(L)]
 
+    # Fourier transform surrogates.
     elif method == _FT:
         b = 2 * np.pi
 
@@ -88,20 +98,21 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
         eta = b * np.random.rand(N, L2 - 1)
 
         ftsig = np.fft.fft(sig, axis=0)
-        ftrp = np.zeros((N, len(ftsig),), dtype=np.complex64)
+        ftrp = np.zeros((N, len(ftsig)), dtype=np.complex64)
         ftrp[:, 0] = ftsig[0]
 
         F = ftsig[1:L2]
-        F = np.tile(F, (N, 1,))
+        F = np.tile(F, (N, 1))
 
-        ftrp[:, 1:L2] = np.multiply(F, np.exp(np.complex(0, 1) * eta))
-        ftrp[:, 1 + L - L2:L] = np.conj(np.fliplr(ftrp[:, 1:L2]))
+        ftrp[:, 1:L2] = F * np.exp(1j * eta)
+        ftrp[:, 1 + L - L2 : L] = np.conj(np.fliplr(ftrp[:, 1:L2]))
 
         surr = np.fft.ifft(ftrp, axis=0)
         surr = np.real(surr)
 
         params.rphases = eta
 
+    # Amplitude-adjusted Fourier transform surrogates.
     elif method == _AAFT:
         b = 2 * np.pi
         eta = b * np.random.rand(N, L2 - 1)
@@ -121,7 +132,7 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
         surr = np.zeros((N, len(sig)), dtype=np.complex)
         surr[:, 0] = gn[:, 0]
         surr[:, 1:L2] = np.multiply(F, np.exp(np.complex(0, 1) * eta))
-        surr[:, 1 + L - L2:L] = np.conj(np.fliplr(surr[:, 1:L2]))
+        surr[:, 1 + L - L2 : L] = np.conj(np.fliplr(surr[:, 1:L2]))
         surr = np.fft.ifft(surr, axis=0)
 
         ind2 = np.argsort(surr, axis=1)
@@ -132,15 +143,64 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
 
         surr = np.real(surr)
 
-    # TODO: add later
+    # Iterated amplitude-adjusted Fourier transform with exact distribution.
     elif method == _IAFFT1:
-        pass
+        maxit = 1000
+        val = np.sort(sig)
+        ind = np.argsort(sig)
+
+        rankind = np.empty(ind.shape, dtype=np.int)
+        rankind[ind] = np.arange(0, L)
+
+        ftsig = np.fft.fft(sig, axis=0)
+        F = np.tile(ftsig, (N, 1))
+        surr = np.zeros((N, L))
+
+        for j in range(N):
+            surr[j, :] = sig[randperm(L)]
+
+        it = 1
+        irank = rankind.copy()
+        irank = np.tile(irank, (N, 1))
+        irank2 = np.zeros((1, L))
+        oldrank = np.zeros((N, L))
+        iind = np.zeros((N, L))
+        iterf = iind.copy()
+
+        while np.max(np.abs(oldrank - irank), axis=1) != 0 and it < maxit:
+            go = np.max(np.abs(oldrank - irank), axis=1)
+            go_c = go.conj().T
+
+            inc = go_c[go_c != 0].nonzero()
+
+            oldrank = irank.copy()
+            iterf[inc, :] = np.real(np.fft.ifft(np.abs(F[inc, :]), axis=0)) * np.exp(
+                1j * np.angle(np.fft.fft(surr[inc, :], axis=1))
+            )
+
+            iind[inc, :] = np.sort(iterf[inc, :], axis=1)
+            for k in range(inc):
+                irank2[iind[k, :]] = np.arange(0, L)
+                irank[k, :] = irank2.copy()
+                surr[k, :] = val[irank2]
+
+            it += 1
+
+    # Iterated amplitude-adjusted Fourier transform with exact spectrum.
     elif method == _IAFFT2:
         pass
+
+    # Wavelet iterated amplitude adjusted Fourier transform surrogates
     elif method == _WIAFFT:
         pass
+
+    # Time-shifted surrogates.
     elif method == _tshift:
-        pass
+        for sn in range(N):
+            startp = random.randint(1, L - 1)
+            surr[sn, :] = np.hstack([sig[startp:L], sig[:startp]])
+
+    # Cycle phase permutation surrogates.
     elif method == _CPP:
         signal = np.mod(sig, 2 * np.pi)
 
@@ -151,15 +211,15 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
             cycles = np.zeros(NC)
 
             for k in range(NC):
-                cycles[k] = signal[dcpoints[k] + 1: dcpoints[k + 1]]
+                cycles[k] = signal[dcpoints[k] + 1 : dcpoints[k + 1]]
 
-            stcycle = signal[:dcpoints[0]]
-            endcycle = signal[dcpoints[k + 1] + 1:]
+            stcycle = signal[: dcpoints[0]]
+            endcycle = signal[dcpoints[k + 1] + 1 :]
 
             for sn in range(N):
-                surr[sn, :] = np.unwrap(np.hstack([
-                    stcycle, cycles[np.random.permutation(NC), endcycle]
-                ]))
+                surr[sn, :] = np.unwrap(
+                    np.hstack([stcycle, cycles[np.random.permutation(NC), endcycle]])
+                )
 
         else:
             for sn in range(N):
@@ -181,39 +241,43 @@ def surrogate_calc(time_series: Union[TimeSeries, ndarray], N, method, pp, fs):
     return surr, params
 
 
-def preprocessing(sig, fs):
+def preprocessing(sig: ndarray, fs: float) -> Tuple[ndarray, ndarray, ndarray, float]:
     sig -= np.mean(sig)
-    t = np.linspace(0, len(sig), len(sig) / fs)
+    t = np.linspace(0, len(sig), int(len(sig) // fs))
     L = len(sig)
+
+    # Find pair of points which minimises mismatch between p consecutive points and the beginning and end of the signal
     p = 10
 
-    K1 = np.round(L / 100)
+    K1 = np.round(L / 100)  # Proportion of signal to consider at beginning.
     k1 = sig[:K1]
-    K2 = np.round(L / 10)
-    k2 = sig[:K2]
+    K2 = np.round(L / 10)  # Proportion of signal to consider at end.
+    k2 = sig[-K2:]
 
+    # Truncate to match start and end points and first derivatives.
     if len(k1) <= p:
         p = len(k1) - 1
 
-    d = np.zeros(len(k1) - p, len(k2) - p)
+    d = np.zeros((len(k1) - p, len(k2) - p))
 
     for j in range(len(k1) - p):
         for k in range(len(k2) - p):
-            d[j, k] = np.sum(np.abs(k1[j:j + p]) - k2[k:k + p])
+            d[j, k] = np.sum(np.abs(k1[j : j + p]) - k2[k : k + p])
 
-    v, I = np.min(np.abs(d), [], 2)
-    _, I2 = np.min(v)
+    abs_d = np.abs(d)
+    v = np.min(abs_d, axis=1)
+    I = np.argmin(abs_d, axis=1)
+    I2 = np.argmin(v)  # Minimum mismatch.
 
     kstart = I2
     kend = I[I2] + len(sig[:-k2])
-    cutsig = sig[kstart:kend]
-    t2 = t[kstart:kend]
+    cutsig = sig[kstart:kend]  # New truncated time series.
+    t2 = t[kstart:kend]  # Corresponding time.
 
     return cutsig, t2, kstart, kend
 
 
 class Params:
-
     def __init__(self):
         self.origsig = None
         self.numsurr = None
