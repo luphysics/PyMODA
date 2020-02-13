c715971a85d486962ab2ff03e27c1d7d8a842326
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-02-13 20:17:29 +0000

    Save data in wavelet phase coherence

--- src/gui/windows/phasecoherence/PCPresenter.py
+++ src/gui/windows/phasecoherence/PCPresenter.py
@@ -14,8 +14,9 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
 import asyncio
-from typing import Tuple
+from typing import Tuple, List, Dict, Optional
 
+import numpy as np
 from PyQt5.QtWidgets import QListWidgetItem
 
 from gui.dialogs.FrequencyDialog import FrequencyDialog
@@ -26,6 +27,8 @@ from maths.signals.SignalPairs import SignalPairs
 from maths.signals.TimeSeries import TimeSeries
 from maths.signals.data.TFOutputData import TFOutputData
 from processes.MPHandler import MPHandler
+from utils.decorators import override
+from utils.dict_utils import sanitise
 
 
 class PCPresenter(BaseTFPresenter):
@@ -37,11 +40,11 @@ class PCPresenter(BaseTFPresenter):
         self.view: PCWindow = self.view
         self.is_calculating_all = True
 
-    def calculate(self, calculate_all: bool):
+    def calculate(self, calculate_all: bool) -> None:
         asyncio.ensure_future(self.coro_calculate(calculate_all))
         print("Started calculation...")
 
-    async def coro_calculate(self, calculate_all: bool):
+    async def coro_calculate(self, calculate_all: bool) -> None:
         self.view.enable_save_data(False)
         self.is_calculating_all = calculate_all
 
@@ -57,6 +60,8 @@ class PCPresenter(BaseTFPresenter):
         self.view.amplitude_plot().set_in_progress(True)
 
         params = self.get_params(all_signals=calculate_all)
+        self.params = params
+
         self.surrogate_count = self.view.get_surr_count()
         self.surrogates_enabled = self.view.get_surr_enabled()
 
@@ -85,21 +90,26 @@ class PCPresenter(BaseTFPresenter):
         self.on_all_tasks_completed()
         print("Finished calculating phase coherence.")
 
-    async def coro_phase_coherence(self, signals, params, on_progress):
+    async def coro_phase_coherence(self, signals, params, on_progress) -> List[tuple]:
         print("Finished wavelet transform. Calculating phase coherence...")
         return await self.mp_handler.coro_phase_coherence(signals, params, on_progress)
 
     def on_transform_completed(
         self, name, times, freq, values, ampl, powers, avg_ampl, avg_pow, opt=None
-    ):
+    ) -> None:
         print(f"Calculated wavelet transform for '{name}'")
+
+        if opt:
+            self.params.set_item("fmin", opt.get("fmin"))
 
         t = self.signals.get(name)
         t.output_data = TFOutputData(
             times, values, ampl, freq, powers, avg_ampl, avg_pow
         )
 
-    def on_phase_coherence_completed(self, signal_pair, tpc, pc, pdiff, surrogate_avg):
+    def on_phase_coherence_completed(
+        self, signal_pair, tpc, pc, pdiff, surrogate_avg
+    ) -> None:
         s1, s2 = signal_pair
 
         d = s1.output_data
@@ -110,7 +120,9 @@ class PCPresenter(BaseTFPresenter):
         sig = self.signals.get(s1.name)
         sig.output_data = d
 
-    def plot_phase_coherence(self):
+        self.enable_save_data(True)
+
+    def plot_phase_coherence(self) -> None:
         data = self.get_selected_signal_pair_data()
         times = data.times
         freq = data.freq
@@ -132,7 +144,7 @@ class PCPresenter(BaseTFPresenter):
         ampl.update_ylabel("Frequency (Hz)")
         ampl.plot(data.overall_coherence, freq, surrogates=data.surrogate_avg)
 
-    def load_data(self):
+    def load_data(self) -> None:
         self.signals = SignalPairs.from_file(self.open_file)
 
         if not self.signals.has_frequency():
@@ -144,11 +156,67 @@ class PCPresenter(BaseTFPresenter):
             else:
                 raise Exception("Frequency was None. Perhaps it was mistyped?")
 
-    def on_data_loaded(self):
+    @override
+    def get_data_to_save(self) -> Optional[Dict]:
+        if not self.params:
+            return None
+
+        output_data: List[TFOutputData] = [
+            s[0].output_data for s in self.signals.get_pairs()
+        ]
+        cols = len(output_data)
+
+        first = [d for d in output_data if d.is_valid()][0]
+
+        coh = np.empty((*first.phase_coherence.shape, cols))
+        avg_coh = np.empty((first.overall_coherence.shape[0], cols))
+
+        avg_surrogates = np.empty((len(first.surrogate_avg), cols))
+
+        amp = np.empty((*first.ampl.shape, cols))
+        avg_amp = np.empty((first.avg_ampl.shape[0], cols))
+
+        freq = first.freq
+        time = first.times
+
+        preproc = []  # TODO: Save this
+
+        for index, d in enumerate(output_data):
+            if d.is_valid():
+                avg_surrogates[:, index] = d.surrogate_avg[:, 0]
+
+                coh[:, :, index] = d.phase_coherence[:, :]
+                avg_coh[:, index] = d.overall_coherence[:, index]
+
+                amp[:, :, index] = d.ampl[:, :]
+                avg_amp[:, index] = d.avg_ampl[:]
+            else:
+                avg_surrogates[:, index] = np.NAN
+
+                amp[:, :, index] = np.NAN
+                coh[:, :, index] = np.NAN
+
+                avg_amp[:, index] = np.NAN
+                avg_coh[:, index] = np.NAN
+
+        pc_data = {
+            "coherence": coh,
+            "avg_coherence": avg_coh,
+            "avg_surrogates": avg_surrogates,
+            "amplitude": amp,
+            "avg_amplitude": avg_amp,
+            "frequency": freq,
+            "time": time,
+            "preprocessed_signals": preproc,
+            **self.params.items_to_save(),
+        }
+        return {"PCData": sanitise(pc_data)}
+
+    def on_data_loaded(self) -> None:
         self.view.update_signal_listview(self.signals.get_pair_names())
         self.plot_signal_pair()
 
-    def plot_signal_pair(self):
+    def plot_signal_pair(self) -> None:
         self.view.plot_signal_pair(self.get_selected_signal_pair())
 
     def get_selected_signal_pair(self) -> Tuple[TimeSeries, TimeSeries]:
@@ -157,7 +225,7 @@ class PCPresenter(BaseTFPresenter):
     def get_selected_signal_pair_data(self) -> TFOutputData:
         return self.get_selected_signal_pair()[0].output_data
 
-    def on_signal_selected(self, item):
+    def on_signal_selected(self, item) -> None:
         if isinstance(item, QListWidgetItem):
             name = item.text()
         else:
--- src/maths/params/PCParams.py
+++ src/maths/params/PCParams.py
@@ -13,31 +13,35 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program. If not, see <https://www.gnu.org/licenses/>.
-from maths.signals.Signals import Signals
+from typing import Dict
+
 from maths.params.TFParams import TFParams, _wft
+from maths.signals.Signals import Signals
+from utils.decorators import override
+from utils.dict_utils import sanitise
 
 
 class PCParams(TFParams):
-
-    def __init__(self,
-                 signals: Signals,
-                 fmin=0,
-                 fmax=None,
-                 fstep="auto",
-                 f0=1,
-                 padding="predictive",
-                 cut_edges=False,
-                 window="Gaussian",
-                 wavelet="Lognorm",
-                 preprocess=True,
-                 rel_tolerance=0.01,
-                 transform=_wft,
-
-                 # Parameters not passed to Matlab.
-                 surr_enabled=False,
-                 surr_count=0,
-                 surr_method="RP",
-                 surr_preproc=False):
+    def __init__(
+        self,
+        signals: Signals,
+        fmin=0,
+        fmax=None,
+        fstep="auto",
+        f0=1,
+        padding="predictive",
+        cut_edges=False,
+        window="Gaussian",
+        wavelet="Lognorm",
+        preprocess=True,
+        rel_tolerance=0.01,
+        transform=_wft,
+        # Parameters not passed to Matlab.
+        surr_enabled=False,
+        surr_count=0,
+        surr_method="RP",
+        surr_preproc=False,
+    ):
         if not surr_enabled:
             surr_count = 0
 
@@ -45,15 +49,24 @@ class PCParams(TFParams):
         self.surr_method = surr_method
         self.surr_preproc = surr_preproc
 
-        super().__init__(signals,
-                         fmin,
-                         fmax,
-                         fstep,
-                         f0,
-                         padding,
-                         cut_edges,
-                         window,
-                         wavelet,
-                         preprocess,
-                         rel_tolerance,
-                         transform)
+        super().__init__(
+            signals,
+            fmin,
+            fmax,
+            fstep,
+            f0,
+            padding,
+            cut_edges,
+            window,
+            wavelet,
+            preprocess,
+            rel_tolerance,
+            transform,
+        )
+
+    @override
+    def items_to_save(self) -> Dict:
+        tf = super().items_to_save()
+
+        out = {"surr_count": self.surr_count, "surr_type": self.surr_method, **tf}
+        return sanitise(out)
